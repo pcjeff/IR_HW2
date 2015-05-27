c435157f318476746b1a988a0a4ae37c0d34fc51
jeff Lin <pcjeff@cml11.csie.ntu.edu.tw>
2015-05-27 23:06:21 +0800

    much faster version but not accurate

--- hw2.py
+++ hw2.py
@@ -9,19 +9,27 @@ fi = 10**-6
 
 def page_rank(graph_size, trans_table):
     iteration = 0
+    no_outlink_pagerank_sum = 0
     rank_vec = []
     rank_vec.append(numpy.ones((graph_size,)))
     rank_vec.append(numpy.zeros((graph_size,)))
+    no_outlink_node = []
+    for i in xrange(1, graph_size+1):
+        if trans_table[i] == []:
+            no_outlink_node.append(i)
     while True:
         start = time.time()
         for i in xrange(1, graph_size+1):
-            if trans_table[i]==[]:
-                #print str(i) + ' : ' + str(rank_vec[(iteration)%2][i-1])
-                rank_vec[(iteration+1)%2] += rank_vec[(iteration%2)][i-1]/graph_size
-            else:
+            if trans_table[i] != []:
                 outlink_num = trans_table[i][0]
                 for out_linked_node in trans_table[i][1:]:
                     rank_vec[(iteration+1)%2][out_linked_node-1] += rank_vec[(iteration%2)][i-1]/outlink_num
+        
+        no_outlink_pagerank_sum = 0
+        for index in no_outlink_node:
+            no_outlink_pagerank_sum += rank_vec[(iteration)%2][index-1]
+        
+        rank_vec[(iteration+1)%2] += no_outlink_pagerank_sum/graph_size
         rank_vec[(iteration+1)%2] = 0.15 + 0.85*rank_vec[(iteration+1)%2]
         end = time.time()
         print 'iteration.' + str(iteration) + ': ' + str(numpy.linalg.norm(rank_vec[0]-rank_vec[1]))
