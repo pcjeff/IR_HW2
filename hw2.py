832f021f1f912f4ffe096171faf79d43d1fe517c
jeff Lin <pcjeff@cml11.csie.ntu.edu.tw>
2015-05-29 14:42:25 +0800

    v2, node without outlink not distribute its pagerank to itself

--- hw2.py
+++ hw2.py
@@ -19,21 +19,22 @@ def page_rank(graph_size, trans_table):
             no_outlink_node.append(i)
     while True:
         start = time.time()
+        no_outlink_pagerank_sum = 0
         for i in xrange(1, graph_size+1):
             if trans_table[i] != []:
                 outlink_num = trans_table[i][0]
                 for out_linked_node in trans_table[i][1:]:
                     rank_vec[(iteration+1)%2][out_linked_node-1] += rank_vec[(iteration%2)][i-1]/outlink_num
-        
-        no_outlink_pagerank_sum = 0
-        for index in no_outlink_node:
-            no_outlink_pagerank_sum += rank_vec[(iteration)%2][index-1]
-        
-        rank_vec[(iteration+1)%2] += no_outlink_pagerank_sum/graph_size
-        rank_vec[(iteration+1)%2] = 0.15 + 0.85*rank_vec[(iteration+1)%2]
+                #with outlink
+            else:
+                rank_vec[(iteration+1)%2][i-1] -= rank_vec[(iteration)%2][i-1]/(graph_size-1)
+                no_outlink_pagerank_sum += rank_vec[(iteration)%2][i-1]
+                #no outlink 
+        rank_vec[(iteration+1)%2] += no_outlink_pagerank_sum/(graph_size-1)
+        rank_vec[(iteration+1)%2] = (1-d) + d*rank_vec[(iteration+1)%2]
         end = time.time()
         print 'iteration.' + str(iteration) + ': ' + str(numpy.linalg.norm(rank_vec[0]-rank_vec[1]))
-        print 'time_total' + str(end-start)
+        print 'time_total: ' + str(end-start)
         if numpy.linalg.norm(rank_vec[0]-rank_vec[1]) < fi:
             return rank_vec[(iteration+1)%2]
         else:
@@ -43,6 +44,7 @@ def page_rank(graph_size, trans_table):
 
 def main():
     path = sys.argv[1]
+    file_name = sys.argv[2]
     #path to the file
     trans_table = defaultdict(list)
     #trans_table
@@ -54,7 +56,7 @@ def main():
             node_id, link_info = line.strip().split(':')
             trans_table[int(node_id)] = map(int,link_info.strip().split())
     
-    with open('output.pagerank', 'w') as f: 
+    with open(file_name, 'w') as f: 
         print 'Computing page rank...'
         rank_vec = page_rank(graph_size, trans_table)
         print 'Writing page rank...'
